ce882ab18753ddc27a74b37eb1ec531630a1ec4a
arthur <arthur.faugeras@gmail.com>
2016-02-07 15:08:56 +0100

    Ajout du prix dans les composants et services liés aux hôtels

--- ts/hotelImpl.ts
+++ ts/hotelImpl.ts
@@ -2,12 +2,15 @@
  * Created by mclaveau on 07/02/2016.
  */
 import {HotelInterface} from "./hotelInterface";
-export class HotelImpl implements HotelInterface{
-    name : string;
-    location : string;
+export class HotelImpl implements HotelInterface {
+    name:string;
+    location:string;
+    price:number;
 
-    constructor(
-        name : string,
-    location : string){}
+    constructor(name:string,
+                location:string,
+                price:number) {
+    }
+
 
 }
--- ts/hotelInterface.ts
+++ ts/hotelInterface.ts
@@ -4,4 +4,5 @@
 export interface HotelInterface {
     name : string;
     location : string;
+    price : number;
 }
